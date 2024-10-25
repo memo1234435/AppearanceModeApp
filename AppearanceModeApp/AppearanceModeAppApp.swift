3d5af2b229531944a616284457ded490b7a17c73
Madoka <madoka3464@gmail.com>
2024-10-25 16:06:45 +0900

    Initial Commit

---
+++ AppearanceModeApp/AppearanceModeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppearanceModeAppApp.swift
+//  AppearanceModeApp
+//
+//  Created by Madoka Suzuki on 2024/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AppearanceModeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppearanceModeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppearanceModeApp
+//
+//  Created by Madoka Suzuki on 2024/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
